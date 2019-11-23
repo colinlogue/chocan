e59128566adc118c68be46e001068b6e8c0186b6
dehine <davidemersonhine@gmail.com>
2019-11-23 12:30:33 -0800

    deh test2 11/23

--- src/DataSource.java
+++ src/DataSource.java
@@ -2,11 +2,7 @@
 // and retrieve from the database.
 
 public class DataSource {
-<<<<<<< HEAD
 	//test test -DEH
-=======
 
-    // all rows in all db tables can be identified by a unique int
-    protected int ident;
->>>>>>> 5f0be635a1fd20f09fe05b2625e71ee6ea756169
+    // all rows in all db tables can be identified by a unique in
 }

f5cbe8ff7544a0889d508cff214085cbf353cd2a
Colin Logue <colinlogue@gmail.com>
2019-12-04 20:25:14 -0800

    updated ProviderDirectory

--- src/ProviderDirectory.java
+++ src/ProviderDirectory.java
@@ -4,15 +4,42 @@ Database access class for services.
 
  */
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ProviderDirectory extends DataSource {
 
-    private int ident_len;
-
-    private String ident_to_string(int num) {
-        return DataSource.ident_to_string(num, ident_len);
+    // Looks up a
+    // Throws a SQLException if the service_code is not found
+    public static Service lookup(int service_code) throws SQLException {
+        String sql = "select * from service where ServiceCode = ?";
+        try (Connection conn = connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setInt(1, service_code);
+            try (ResultSet results = pstmt.executeQuery()) {
+                Service service = new Service();
+                service.fee = results.getInt("fee");
+                service.label = results.getString("label");
+                service.service_code = service_code;
+                return service;
+            }
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
     public void display() {
 
+    }
+
+    public static void main(String[] args) {
+        try {
+            Service s = ProviderDirectory.lookup(103050);
+            System.out.println(s.label);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }

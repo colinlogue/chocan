c771c0d7ec003c4b2e83cadb39876943ecaa0ede
kaigoos <skrillznix@gmail.com>
2019-12-06 20:07:31 -0800

    Change retrieve test to two tests

--- tests/AddressDataTest.java
+++ tests/AddressDataTest.java
@@ -1,10 +1,9 @@
 import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
-
 import java.sql.SQLException;
-
 import static junit.framework.TestCase.*;
+
 public class AddressDataTest
 {
     @Before
@@ -17,19 +16,26 @@ public class AddressDataTest
         TestConfig.reset_db();
     }
 
-
     @Test(expected = SQLException.class)
-    public void test_retrieve() throws SQLException
+    public void test_retrieve_failure() throws SQLException{
+        int invalid_id = -1;
+        AddressData.retrieve(invalid_id);
+    }
+
+    @Test
+    public void test_retrieve()
     {
         int valid_provider_add_id = 5;
         int valid_member_add_id = 12;
-        int invalid_id = -1;
         AddressData providerData = new AddressData("Orange Ave.", "Portland", "OR", "91234");
         AddressData memberData = new AddressData("304 Avenue Q", "Portland", "OR", "90809");
 
-        assertEquals(providerData, AddressData.retrieve(valid_provider_add_id));
-        assertEquals(memberData, AddressData.retrieve(valid_member_add_id));
-        AddressData.retrieve(invalid_id);
+        try{
+            assertEquals(providerData, AddressData.retrieve(valid_provider_add_id));
+            assertEquals(memberData, AddressData.retrieve(valid_member_add_id));
+        } catch(SQLException e){
+            return;
+        }
     }
 
     @Test

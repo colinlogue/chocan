5570bc52cbf8b0cb773720bea2a3840c43e5a1df
dehine <davidemersonhine@gmail.com>
2019-12-06 20:03:03 -0800

    new tests

--- tests/ProviderDataTest.java
+++ tests/ProviderDataTest.java
@@ -18,3 +18,42 @@ public class ProviderDataTest
         TestConfig.reset_db();
     }
 
+    int valid_provider_id = 900001;
+    int v_id_1 = 900003;
+    int invalid_provider_id = 999;
+    int i_id_1 = 555;
+    int i_id_2 = 444;
+    ProviderData p_data = new ProviderData("Dr Bill", "333 Evergreen Terrace", "Springfield", "IL", "91234");
+
+    @Test
+    public void test_retrieve_failure() throws SQLException
+    {
+        ProviderData providerData = new ProviderData();
+        try{
+            assertNull(providerData.retrieve(invalid_provider_id));
+        }catch (SQLException e){
+            return;
+        }
+    }
+
+    @Test
+    void test_retrieve_success() throws SQLException
+    {
+        ProviderData providerData = new ProviderData();
+        try{
+            assertNotNull(providerData.retrieve(valid_provider_id));
+        }catch (SQLException e){
+            return;
+        }
+    }
+
+    @Test
+    void test_write() throws SQLException
+    {
+        assertTrue(p_data.write());
+    }
+
+
+
+
+}

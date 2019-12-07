fb4e0aced9370bfee77c77047ba787fbef7bd8b7
kaigoos <skrillznix@gmail.com>
2019-12-06 20:30:04 -0800

    ManagerReportTest in progress

--- tests/ManagerReportTest.java
+++ tests/ManagerReportTest.java
@@ -1,14 +1,24 @@
 import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
+import java.sql.SQLException;
 
 public class ManagerReportTest
 {
     ProviderData providerData = new ProviderData("","Orange Ave.", "Portland", "OR", "91234");
+    int valid_provider_add_id = 5;
 
     @Test
     public void test_retrieve_member(){
-        MemberData memberData = new MemberData("","304 Avenue Q", "Portland", "OR", "90809");
+        int valid_member_add_id = 12;
+        MemberData memberData = new MemberData("Colin Logue","304 Avenue Q", "Portland", "OR", "90809");
+        MemberData other_member;
+
+        try{
+            other_member = MemberData.retrieve(valid_member_add_id);
+        } catch(SQLException e){
+            return;
+        }
 
     }
 }
